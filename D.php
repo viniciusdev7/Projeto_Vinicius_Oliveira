919935218005bceb51e436513d5ec283cf6a3cc6
Vinícius Oliveira <vinicius.oli7dev@gmail.com>
2025-04-17 14:27:45 -0300

    Criando classe D com metodos MD1() e MD2()

---
+++ D.php
@@ -0,0 +1,29 @@
+<?php
+
+class D {
+    string D1;
+    int D2;
+
+    public function setD1() {
+        return D1;
+    }       
+    public function getD1() {
+        return D1;
+    }
+    public function setD2() {
+        return D2;
+    }
+    public function getD2() {
+        return D2;
+    }
+
+    public function MD1(){
+        echo "Metodo - MD1";
+    }
+
+    public function MD2(){
+        echo "Metodo - MD2";
+    }
+
+}
+
